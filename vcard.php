8a1a1c21e162dc56d794157f7ac56684315f9849
Elan Ruusamäe <glen@delfi.ee>
2012-07-19 21:51:57 +0300

    avoid conflict with core quoted_printable_encode
    quoted_printable_encode is included in PHP 5.3.0

--- vcard.php
+++ vcard.php
@@ -34,6 +34,7 @@ function escape($string) {
 }
 
 // taken from PHP documentation comments
+if (!function_exists('quoted_printable_encode')) {
 function quoted_printable_encode($input, $line_max = 76) {
 	$hex = array('0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F');
 	$lines = preg_split("/(?:\r\n|\r|\n)/", $input);
@@ -65,6 +66,7 @@ function quoted_printable_encode($input, $line_max = 76) {
 		if ($j<count($lines)-1) $output .= $linebreak;
 	}
 	return trim($output);
+}
 }
 
 class vCard {

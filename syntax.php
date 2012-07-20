a8bf25877b17c620e296a81769d67fd946f2d9d8
Elan Ruusamäe <glen@delfi.ee>
2012-07-20 12:35:15 +0300

    depend on dokuwiki autoloader

--- syntax.php
+++ syntax.php
@@ -12,9 +12,8 @@
  * @author     Elan Ruusamäe <glen@delfi.ee>
  */
 
-if(!defined('DOKU_INC')) define('DOKU_INC',realpath(dirname(__FILE__).'/../../').'/');
-if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-require_once(DOKU_PLUGIN.'syntax.php');
+// must be run within Dokuwiki
+if(!defined('DOKU_INC')) die();
 
 /**
  * All DokuWiki plugins to extend the parser/rendering mechanism

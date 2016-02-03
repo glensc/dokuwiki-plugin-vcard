fa1af851b5a03afb90c34774e32bdbd386e464c8
Andreas Gohr <andi@splitbrain.org>
2016-02-03 12:52:15 +0100

    Adjust method signatures to match parent

--- syntax.php
+++ syntax.php
@@ -37,7 +37,7 @@ class syntax_plugin_vcard extends DokuWiki_Syntax_Plugin {
 	/**
 	 * Handle the match
 	 */
-	function handle($match, $state, $pos, &$handler) {
+	function handle($match, $state, $pos, Doku_Handler $handler) {
 		// strip markup
 		$match = substr($match, 8, -2);
 
@@ -113,7 +113,7 @@ class syntax_plugin_vcard extends DokuWiki_Syntax_Plugin {
 	/**
 	 * Create output
 	 */
-	function render($mode, &$renderer, $data) {
+	function render($mode, Doku_Renderer $renderer, $data) {
 		if ($mode != 'xhtml') {
 			return false;
 		}
